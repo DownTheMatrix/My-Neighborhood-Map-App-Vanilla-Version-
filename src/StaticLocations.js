4601663137bf1f973606287424cd5b0d43d577fd
DownTheMatrix <suija87@gmail.com>
2018-08-10 17:27:58 +0200

    Change key names

--- src/StaticLocations.js
+++ src/StaticLocations.js
@@ -1,41 +1,48 @@
+/* Data retrieved from: https://api.foursquare.com/v2/venues/search?near=Verona&query=restaurant&category=4bf58dd8d48988d12d941735&limit=5&radius=5000&intent=browse&client_id=DBRDMEKKFERTVRYWMB5DV2EZZKBVYEPTSEZCKNJGXSQ1PI3H&client_secret=Z3AYSBO30DL1XA4FFTMR2Z314A5MVIZZSB2M0EVFJEAY5GBZ&v=20180101&locale=en */
+
 export const StaticLocations = [{
     locationCoords: {
-      lat: 45.441941,
-      lng: 10.998373
+      lat: 45.42422,
+      lng: 10.991686
     },
-    locationName: 'Juliet House',
+    locationName: "Mizuki Lounge Restaurant",
+    locationId: "5952389dccad6b171c8d3e58",
     address: "",
   },
   {
     locationCoords: {
-      lat: 45.439529,
-      lng: 10.987529
+      lat: 45.448458542692556,
+      lng: 11.00220835305019
     },
-    locationName: 'Castelvecchio',
+    locationName: "TeodoricoRe Restaurant Bar Verona",
+    locationId: "4dcee64f45ddbe15f8956f72",
     address: ""
   },
   {
     locationCoords: {
-      lat: 45.442456,
-      lng: 10.978493
+      lat: 45.438385,
+      lng: 10.991622
     },
-    locationName: 'San Zeno Basilica',
+    locationName: "Hotel Montemezzi Restaurant",
+    locationId: "59c1f834a2a6ce4762f1de1e",
     address: ""
   },
   {
     locationCoords: {
-      lat: 45.447458,
-      lng: 11.001737
+      lat: 45.44499306798319,
+      lng: 10.998014420366752
     },
-    locationName: 'Archeological Museum',
+    locationName: "AMO Opera Restaurant",
+    locationId: "52630778498ef9cb50326fb7",
     address: ""
   },
   {
     locationCoords: {
-      lat: 45.438889,
-      lng: 10.994444
+      lat: 45.44232305284876,
+      lng: 10.99606990814209
     },
-    locationName: 'Arena',
+    locationName: "Sun Restaurant",
+    locationId: "5590d1da498e4edbe573034b",
     address: ""
   }
 ];
