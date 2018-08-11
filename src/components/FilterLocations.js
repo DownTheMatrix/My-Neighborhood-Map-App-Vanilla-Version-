a07cba4a1253d7a7e6bd8c38e7f9054b76c60866
DownTheMatrix <suija87@gmail.com>
2018-08-11 16:59:53 +0200

    Create FilterLocations component

---
+++ src/components/FilterLocations.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import Input from "@material-ui/core/Input";  /* src: https://github.com/mui-org/material-ui/blob/master/docs/src/pages/demos/text-fields/Inputs.js */
+
+/* Input component style */
+const styles = theme => ({
+    container: {
+      display: "flex",
+      flexWrap: "wrap"
+    },
+    input: {
+      margin: theme.spacing.unit,
+      color: "#fff",
+      borderBottom: "1px solid #000",
+      width: "100%"
+    },
+  });
+
+function FilterLocations ( props ) {
+
+    const { classes } = props;
+
+    return (
+        <div className = { classes.container }>
+            <form aria-label="search your location">
+                <Input
+                    className = { classes.input }
+                    type = "text"
+                    placeholder = { "Search for your venue" }
+                    inputProps = {{ "aria-label" : "Query" }}
+                />
+            </form>
+        </div>
+    );
+}
+
+export default withStyles(styles)(FilterLocations);
