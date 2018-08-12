3534082e9672738aa27986bf11e8621ecfcb47ad
DownTheMatrix <suija87@gmail.com>
2018-08-12 17:37:20 +0200

    Add hamburger button

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ function Header( props ) {
     return (
         <header id="header">
             <h1>Verona Explorer</h1>
+            <button className = { "hamburger-button" } onClick = { props.onClick }>Toggler</button>
         </header>
     );
 }
